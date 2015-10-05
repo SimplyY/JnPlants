51fb1d3bc39ec4519ea7da4647f00d5acded06bf
SimplyY <SimplyYu@163.com>
2015-10-05 22:32:04 +0800

    add readme and solve cross domain error

--- web/image-bed/image-bed.js
+++ web/image-bed/image-bed.js
@@ -1,8 +1,14 @@
 /*global Qiniu */
 /*global plupload */
-/*global FileProgress */
 // global jq
 
+// if you use it please README, all are very important
+// 0. requrie qiniu, plupload, jq, like image-bew.html
+// 1. write html  need a button id='pickfiles', and button's container id = 'upload-image'
+// 2. text-input and preivew just like how to use markdown.js
+// 3. upload max_file_size: '2mb'
+
+// 从服务器端，获取七牛云的uptoken
 $.get("http://121.40.224.83:19110/get-qiniu-uptoken", function(data) {
     var My_Uptoken = data.uptoken;
 
@@ -12,19 +18,18 @@ $.get("http://121.40.224.83:19110/get-qiniu-uptoken", function(data) {
             domain: '7xkpdt.com1.z0.glb.clouddn.com',
             runtimes: 'html5,flash,html4',
             browse_button: 'pickfiles',
-            container: 'load-image',
+            container: 'upload-image',
             drop_element: 'container',
-            max_file_size: '4mb',
-            flash_swf_url: 'https://dn-cdnjscn.qbox.me/ajax/libs/plupload/2.1.5/Moxie.swf',
             dragdrop: true,
-            chunk_size: '2mb',
+            max_file_size: '2mb',
+            flash_swf_url: 'https://dn-cdnjscn.qbox.me/ajax/libs/plupload/2.1.5/Moxie.swf',
             get_new_uptoken: false,
-
             auto_start: true,
             init: {
                 'FilesAdded': function(up, files) {
                     plupload.each(files, function(file) {
-                        isImg(file.name);
+                        checkIsImg(file.name);
+                        checkSize(file.size);
                     });
                 },
                 'FileUploaded': function(up, file, info) {
@@ -66,15 +71,24 @@ $.get("http://121.40.224.83:19110/get-qiniu-uptoken", function(data) {
         $('#preview').html(markdown.toHTML(insertMd));
     }
 
-    function isImg(fileName) {
+    function checkIsImg(fileName) {
         // 确保图片格式
         var imgNameArray = ['jpeg', 'png', 'jpg'];
+
         var suffixs = fileName.split('.')[fileName.split('.').length - 1];
         if (_.indexOf(imgNameArray, suffixs) > -1) {
             return true;
         } else {
             alert('请上传图片, 且格式为' + imgNameArray.toString());
             return false;
+        }
+    }
+
+//  meta size 1byte
+    function checkSize(fileSize) {
+        var sizeMb = fileSize/1024/1024;
+        if (sizeMb > 2) {
+            alert("上传图片不能大于2mb");
         }
     }
 });
