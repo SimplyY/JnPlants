ade524555db169b48f304ed6830e3a4404196a7f
putiwangsheng <2026929156@qq.com>
2015-10-07 14:32:17 +0800

    single web:scroll

--- web/scene/js/myscroll.js
+++ web/scene/js/myscroll.js
@@ -1,5 +1,10 @@
 function loaded() {
-    var myScroll = new IScroll('.wrapper', {
+    var myScroll = new IScroll('.s-scroller', {
+        mouseWheel: true,
+        scrollbars: true
+    });
+
+    var mycroll = new IScroll('.c-scroller', {
         mouseWheel: true,
         scrollbars: true
     });
