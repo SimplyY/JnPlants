24d0002222ff2368d33b10d4d2606ccf480bfa19
SimplyY <SimplyYu@163.com>
2015-11-08 22:24:23 +0800

    change fis-conf to bundle.js

--- web/scene/fis-conf.js
+++ web/scene/fis-conf.js
@@ -1,24 +1,14 @@
-fis.match('*.js', {
+fis.match('bundle.js', {
     // fis-optimizer-uglify-js 插件进行压缩，已内置
     optimizer: fis.plugin('uglify-js')
 });
 
-fis.match('*.css', {
-    // fis-optimizer-clean-css 插件进行压缩，已内置
-    optimizer: fis.plugin('clean-css')
-});
-
 fis.match('::package', {
-  postpackager: fis.plugin('loader',{
-      allInOne: true
-  })
+    postpackager: fis.plugin('loader', {
+        allInOne: true
+    })
 });
 
-
-fis.match('*.css', {
-  packTo: '/scene/css/aio.css'
-});
-
-fis.match('*.js', {
-  packTo: '/scene/js/aio.js'
+fis.match('bundle.js', {
+    packTo: '/scene/js/aio.js'
 });
