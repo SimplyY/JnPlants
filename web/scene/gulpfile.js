8561db9519438bf1fdca0617d23ecda67ebff47b
SimplyY <SimplyYu@163.com>
2015-11-13 14:17:37 +0800

    browsync

---
+++ web/scene/gulpfile.js
@@ -0,0 +1,12 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch("./*/*.*").on('change', browserSync.reload);
+});
