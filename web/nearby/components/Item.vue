e77baf059eaa5750b74e4069d11745b1b04d299f
长凄 <2026929156@qq.com>
2016-01-15 20:40:43 +0800

    modify plant page

--- web/nearby/components/Item.vue
+++ web/nearby/components/Item.vue
@@ -1,3 +1,49 @@
 <template>
-    i love lp~
+    <div class="item">
+        <img v-bind:src="item.plant" alt="" />
+        <div class="description">
+            <h4 class="title">
+                {{item.title}}
+            </h4>
+            <p>
+                {{item.distance}}
+            </p>
+        </div>
+    </div>
 </template>
+
+<script>
+export default {
+    name: 'Item',
+
+    props: {
+        item: Object
+    }
+}
+</script>
+
+<style media="screen">
+    .item{
+        width: 47%;
+        display: inline-block;
+        vertical-align: top;
+        margin: 0.4rem 1%;
+        background-color: #fff;
+        border-radius: 1.5%;
+    }
+    .description{
+        padding: 2% 4%;
+        position: relative;
+    }
+    img {
+        max-width: 100%;
+    }
+    p{
+        display: inline-block;
+        font-size: .8rem;
+        color: #898888;
+        position: absolute;
+        right: .5rem;
+        top: .3rem;
+    }
+</style>
--- web/nearby/components/List.vue
+++ web/nearby/components/List.vue
@@ -1,8 +1,10 @@
 <template>
-    <div id="">
-        <item>
+    <div class="list">
+        <item v-for="item in items"
+        :item="item">
         </item>
-
+    </div>
+    <div class="loader">
 
     </div>
 </template>
@@ -13,7 +15,18 @@ import Item from './Item.vue'
 export default {
     components: {
         Item
+    },
+
+    data() {
+        return {
+            items: [{plant: 'http://7xkpdt.com1.z0.glb.clouddn.com/11.jpg?imageMogr2/thumbnail/400x/strip', title: '腊梅', distance: '1.5km'},{plant: 'http://7xkpdt.com1.z0.glb.clouddn.com/11.jpg?imageMogr2/thumbnail/400x/strip', title: '腊梅', distance: '1.5km'},{plant: 'http://7xkpdt.com1.z0.glb.clouddn.com/11.jpg?imageMogr2/thumbnail/400x/strip', title: '腊梅', distance: '1.5km'},{plant: 'http://7xkpdt.com1.z0.glb.clouddn.com/11.jpg?imageMogr2/thumbnail/400x/strip', title: '腊梅', distance: '1.5km'},{plant: 'http://7xkpdt.com1.z0.glb.clouddn.com/11.jpg?imageMogr2/thumbnail/400x/strip', title: '腊梅', distance: '1.5km'},{plant: 'http://7xkpdt.com1.z0.glb.clouddn.com/11.jpg?imageMogr2/thumbnail/400x/strip', title: '腊梅', distance: '1.5km'},{plant: 'http://7xkpdt.com1.z0.glb.clouddn.com/11.jpg?imageMogr2/thumbnail/400x/strip', title: '腊梅', distance: '1.5km'},{plant: 'http://7xkpdt.com1.z0.glb.clouddn.com/11.jpg?imageMogr2/thumbnail/400x/strip', title: '腊梅', distance: '1.5km'},{plant: 'http://7xkpdt.com1.z0.glb.clouddn.com/11.jpg?imageMogr2/thumbnail/400x/strip', title: '腊梅', distance: '1.5km'},{plant: 'http://7xkpdt.com1.z0.glb.clouddn.com/11.jpg?imageMogr2/thumbnail/400x/strip', title: '腊梅', distance: '1.5km'}]
+        }
     }
 }
-
 </script>
+<style media="screen">
+    .list{
+        width: 100%;
+        margin: 0 1%;
+    }
+</style>
